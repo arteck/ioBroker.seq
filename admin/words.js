03061f634f2627b8d36117243c0df9897cd32d9b
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-09-02 00:25:30 +0200

    Update Admin HTML

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,44 @@ systemDictionary = {
 		'pl': 'Ustawienia adaptera dla seq',
 		'zh-cn': 'seq的适配器设置'
 	},
-	'option1': {
-		'en': 'option1',
-		'de': 'Option 1',
-		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'
+	'serverData': {
+		'en': 'Server Data',
+		'de': 'Server Data'
 	},
-	'option2': {
-		'en': 'option2',
-		'de': 'Option 2',
-		'ru': 'option2',
-		'pt': 'opção 2',
-		'nl': 'Optie 2',
-		'fr': 'Option 2',
-		'it': 'opzione 2',
-		'es': 'opcion 2',
-		'pl': 'Opcja 2',
-		'zh-cn': '选项2'
-	}
+	'logLvl': {
+		'en': 'Log level',
+		'de': 'Log Level'
+	},
+	'silly': {
+		'en': 'Verbose',
+		'de': 'Verbose'
+	},
+	'debug': {
+		'en': 'Debug',
+		'de': 'Debug'
+	},
+	'info': {
+		'en': 'Information',
+		'de': 'Information'
+	},
+	'warn': {
+		'en': 'Warning',
+		'de': 'Warning'
+	},
+	'error': {
+		'en': 'Error',
+		'de': 'Error'
+	},
+	'apiKey': {
+		'en': 'APIKey (Optinal)',
+		'de': 'APIKey (Optinal)'
+	},
+	'url': {
+		'en': 'URL',
+		'de': 'URL'
+	},
+	'port': {
+		'en': 'Port',
+		'de': 'Port'
+	},
 };
