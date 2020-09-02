b7831d45cff974d14eecfc2c49525b60c00c05ec
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-09-02 15:50:59 +0200

    update

--- main.js
+++ main.js
@@ -4,128 +4,98 @@
  * Created with @iobroker/create-adapter v1.26.3
  */
 
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require('@iobroker/adapter-core');
+const seq = require('seq-logging');
+var logger;
+var verboseActive;
+var debugActive;
+var infomationActive;
+var warningActive;
+var errorActive;
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
 
 class Seq extends utils.Adapter {
 
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: 'seq',
-		});
-		this.on('ready', this.onReady.bind(this));
-		this.on('stateChange', this.onStateChange.bind(this));
-		// this.on('objectChange', this.onObjectChange.bind(this));
-		// this.on('message', this.onMessage.bind(this));
-		this.on('unload', this.onUnload.bind(this));
-	}
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'seq',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
 
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
+        /*
+		    adapter.requireLog(true);
+			adapter.on('log', function(logObject) {
+				// Here we have the log in "logObject" and can handle it accordingly.
+				const severity = logObject.severity; // the log level (severity): info, warn, error, etc.
+				// ....
+			});
+		*/
+    }
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info('config option1: ' + this.config.option1);
-		this.log.info('config option2: ' + this.config.option2);
+    async onReady() {
 
+        let _serverUrl = this.config.url;
+        let _serverPort = this.config.port;
+        let _apiKey = this.config.apiKey;
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync('admin', 'iobroker');
-		this.log.info('check user admin pw iobroker: ' + result);
+        logger = new seq.Logger({ serverUrl: _serverUrl + ':' + _serverPort, apiKey: _apiKey });
+    }
 
-		result = await this.checkGroupAsync('admin', 'admin');
-		this.log.info('check group user admin group admin: ' + result);
-	}
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            logger.close();
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+}
 
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
+function SeqLog(logLevel, source, message) {
+    logger.emit({
+        timestamp: new Date(),
+        level: logLevel,
+        messageTemplate: '{Source}: ' + message,
+        properties: {
+            Application: 'ioBroker',
+            Source: source
+        }
+    });
+}
 
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
+function createMessageObj(inMessage) {
+    const index = Object.values(indexesOf(inMessage, / /g))[0][1];
+    const message = inMessage.substring(index);
+    return { message: message }
+}
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.message" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === 'object' && obj.message) {
-	// 		if (obj.command === 'send') {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info('send command');
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-	// 		}
-	// 	}
-	// }
-
+function indexesOf(string, regex) {
+    var match, indexes = {};
+    regex = new RegExp(regex);
+    while (match = regex.exec(string)) {
+        if (!indexes[match[0]]) indexes[match[0]] = [];
+        indexes[match[0]].push(match.index);
+    }
+    return indexes;
 }
 
 // @ts-ignore parent is a valid property on module
 if (module.parent) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new Seq(options);
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Seq(options);
 } else {
-	// otherwise start the instance directly
-	new Seq();
+    // otherwise start the instance directly
+    new Seq();
 }
