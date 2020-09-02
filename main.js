d78e68df12a53e9b4b6a8c11f3b1fe29903eabbb
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-09-02 22:57:11 +0200

    save intermediate step

--- main.js
+++ main.js
@@ -6,50 +6,52 @@
 
 const utils = require('@iobroker/adapter-core');
 const seq = require('seq-logging');
-var logger;
-var verboseActive;
-var debugActive;
-var infomationActive;
-var warningActive;
-var errorActive;
+let logger;
+let verboseActive;
+let debugActive;
+let infomationActive;
+let warningActive;
+let errorActive;
 
 
 class Seq extends utils.Adapter {
 
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
     constructor(options) {
         super({
             ...options,
             name: 'seq',
         });
+
+        //this.requireLog(true);
         this.on('ready', this.onReady.bind(this));
         this.on('unload', this.onUnload.bind(this));
-
-        /*
-		    adapter.requireLog(true);
-			adapter.on('log', function(logObject) {
-				// Here we have the log in "logObject" and can handle it accordingly.
-				const severity = logObject.severity; // the log level (severity): info, warn, error, etc.
-				// ....
-			});
-		*/
+        this.on('log', this.onLog.bind(this));
     }
+
 
     async onReady() {
 
-        let _serverUrl = this.config.url;
-        let _serverPort = this.config.port;
-        let _apiKey = this.config.apiKey;
+        const _serverUrl = this.config.url;
+        const _serverPort = this.config.port;
+        const _apiKey = this.config.apiKey;
 
-        logger = new seq.Logger({ serverUrl: _serverUrl + ':' + _serverPort, apiKey: _apiKey });
+        this.log.warn('config : ' + _serverUrl);
+        this.log.warn('config : ' + _serverPort);
+        this.log.warn('config : ' + _apiKey);
+
+        logger = new seq.Logger({
+            serverUrl: _serverUrl + ':' + _serverPort,
+            apiKey: _apiKey
+        });
     }
 
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
+    onLog(data) {
+        this.log.warn('config : ' + data);
+        const _seqLogLvl = this.GetSeqLogLvl(data.severity);
+        const _message = this.createMessageObj(data.message);
+        this.SeqLog(seqLogLvl, data.ts, data.from, data.message);
+    }
+
     onUnload(callback) {
         try {
             logger.close();
@@ -58,34 +60,60 @@ class Seq extends utils.Adapter {
             callback();
         }
     }
-}
 
-function SeqLog(logLevel, source, message) {
-    logger.emit({
-        timestamp: new Date(),
-        level: logLevel,
-        messageTemplate: '{Source}: ' + message,
-        properties: {
-            Application: 'ioBroker',
-            Source: source
+    GetSeqLogLvl(data) {
+        let _seqLogLvl;
+
+        switch (data) {
+            case 'silly':
+                _seqLogLvl = 'Verbose';
+                break;
+            case 'debug':
+                _seqLogLvl = 'Debug';
+                break;
+            case 'info':
+                _seqLogLvl = 'Information';
+                break;
+            case 'warn':
+                _seqLogLvl = 'Warning';
+                break;
+            case 'error':
+                _seqLogLvl = 'Error';
+                break;
+            default:
+                _seqLogLvl = '';
         }
-    });
-}
 
-function createMessageObj(inMessage) {
-    const index = Object.values(indexesOf(inMessage, / /g))[0][1];
-    const message = inMessage.substring(index);
-    return { message: message }
-}
-
-function indexesOf(string, regex) {
-    var match, indexes = {};
-    regex = new RegExp(regex);
-    while (match = regex.exec(string)) {
-        if (!indexes[match[0]]) indexes[match[0]] = [];
-        indexes[match[0]].push(match.index);
+        return _seqLogLvl;
     }
-    return indexes;
+
+    SeqLog(logLevel, ts, from, message) {
+        logger.emit({
+            timestamp: ts,
+            level: logLevel,
+            messageTemplate: '{Source}: ' + message,
+            properties: {
+                Application: 'ioBroker',
+                Source: from
+            }
+        });
+    }
+
+    createMessageObj(inMessage) {
+        const index = Object.values(this.indexesOf(inMessage, / /g))[0][1];
+        const message = inMessage.substring(index);
+        return message;
+    }
+
+    indexesOf(string, regex) {
+        let match, indexes = {};
+        regex = new RegExp(regex);
+        while (match = regex.exec(string)) {
+            if (!indexes[match[0]]) indexes[match[0]] = [];
+            indexes[match[0]].push(match.index);
+        }
+        return indexes;
+    }
 }
 
 // @ts-ignore parent is a valid property on module
@@ -98,4 +126,5 @@ if (module.parent) {
 } else {
     // otherwise start the instance directly
     new Seq();
+
 }
