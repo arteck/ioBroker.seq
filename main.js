fc7bae129fd1f831a84d8b45f4507381cb4525b3
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-09-07 21:48:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 'use strict';
 
-// Todo prüfen ob Config vorhanden...
-// Todo Logausgaben...
-
 const utils = require('@iobroker/adapter-core');
 const seq = require('seq-logging');
 const adapterName = require('./package.json').name.split('.').pop();
 
-let seqLogLvlMap = {
-    silly: { LogLvl: 'Verbose', Active: false },
-    debug: { LogLvl: 'Debug', Active: false },
-    info: { LogLvl: 'Information', Active: false },
-    warn: { LogLvl: 'Warning', Active: false },
-    error: { LogLvl: 'Error', Active: false }
-};
+let seqLogLvlMap = [{
+        LogLvl: 'silly',
+        SeqLogLvl: 'Verbose',
+        Active: false
+    },
+    {
+        LogLvl: 'debug',
+        SeqLogLvl: 'Debug',
+        Active: false
+    },
+    {
+        LogLvl: 'info',
+        SeqLogLvl: 'Information',
+        Active: false
+    },
+    {
+        LogLvl: 'warn',
+        SeqLogLvl: 'Warning',
+        Active: false
+    },
+    {
+        LogLvl: 'error',
+        SeqLogLvl: 'Error',
+        Active: false
+    }
+];
 
 let seqLogger;
 
@@ -27,7 +43,6 @@ class Seq extends utils.Adapter {
 
         this.on('ready', this.onReady.bind(this));
         this.on('unload', this.onUnload.bind(this));
-        this.on('log', this.onLog.bind(this));
     }
 
     onReady() {
@@ -35,37 +50,50 @@ class Seq extends utils.Adapter {
         const _serverPort = this.config.port;
         const _apiKey = this.config.apiKey;
 
-        seqLogLvlMap['silly'].Active = this.config.silly;
-        seqLogLvlMap['debug'].Active = this.config.debug;
-        seqLogLvlMap['info'].Active = this.config.info;
-        seqLogLvlMap['warn'].Active = this.config.warn;
-        seqLogLvlMap['error'].Active = this.config.error;
+
+
+        // Check which log events have been activated
+        let _subscribedEvents = [];
+        seqLogLvlMap.forEach(x => {
+            x.Active = this.config[x.LogLvl];
+            if (x.Active) {
+                _subscribedEvents.push(x.LogLvl);
+            }
+        });
+        this.log.info(`Log events [${_subscribedEvents.join(' ,')}] subscribed`)
+
+        // Check if a log event was activated
+        if (seqLogLvlMap.filter(x => x.Active).length === 0) {
+            this.log.warn('No log events were subscribed, please check your settings!')
+            return;
+        }
 
         this.requireLog(true);
+        this.on('log', this.onLog.bind(this));
 
         seqLogger = new seq.Logger({
             serverUrl: _serverUrl + ':' + _serverPort,
             apiKey: _apiKey
         });
-
     }
 
     onLog(data) {
-        const _seqLogObj = seqLogLvlMap[data.severity];
-
+        const _seqLogObj = seqLogLvlMap.find(x => x.LogLvl === data.severity);
+        const _msgObj = this.ExtractPidAndMessage(data.message);
         if (_seqLogObj.Active) {
             try {
                 seqLogger.emit({
                     timestamp: new Date(data.ts).toISOString(),
-                    level: _seqLogObj.LogLvl,
-                    messageTemplate: '{Source}: ' + this.ExtractMessage(data.message),
+                    level: _seqLogObj.SeqLogLvl,
+                    messageTemplate: '{Source}: ' + _msgObj.Message,
                     properties: {
                         Application: 'ioBroker',
-                        Source: data.from
+                        Source: data.from,
+                        Pid: _msgObj.Pid
                     }
                 });
-            } catch (e) {
-                this.log.error(`Cannot send data to server: ${e}`);
+            } catch (ex) {
+                this.log.error(`Cannot send data to server: ${ex}`);
             }
         }
     }
@@ -74,15 +102,20 @@ class Seq extends utils.Adapter {
         try {
             seqLogger.close();
             callback();
-        } catch (e) {
+        } catch (ex) {
             callback();
         }
     }
 
-    ExtractMessage(inMessage) {
-        const index = Object.values(this.IndexesOf(inMessage, / /g))[0][1];
-        const message = inMessage.substring(index);
-        return message;
+    ExtractPidAndMessage(inMessage) {
+        const _mIndex = Object.values(this.IndexesOf(inMessage, / /g))[0][1];
+        const _pIndex = Object.values(this.IndexesOf(inMessage, / /g))[0][0];
+        const _message = inMessage.substring(_mIndex).trim();
+        const _pid = inMessage.substring(_pIndex, _mIndex).replace('(', '').replace(')', '').trim();
+        return {
+            Message: _message,
+            Pid: parseInt(_pid)
+        };
     }
 
     IndexesOf(string, regex) {
