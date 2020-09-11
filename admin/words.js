87985500871d7f57f660d360e5f82c84ee0e12bc
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-09-11 19:31:36 +0200

    add missing translate and ru translate (google)

--- admin/words.js
+++ admin/words.js
@@ -3,55 +3,85 @@
 
 systemDictionary = {
 	'seq adapter settings': {
-		'en': 'Adapter settings for seq',
-		'de': 'Adaptereinstellungen für seq',
-		'ru': 'Настройки адаптера для seq',
-		'pt': 'Configurações do adaptador para seq',
-		'nl': 'Adapterinstellingen voor seq',
-		'fr': "Paramètres d'adaptateur pour seq",
-		'it': "Impostazioni dell'adattatore per seq",
-		'es': 'Ajustes del adaptador para seq',
-		'pl': 'Ustawienia adaptera dla seq',
+		'en': 'Adapter settings for Seq',
+		'de': 'Adaptereinstellungen für Seq',
+		'ru': 'Настройки адаптера для Seq',
+		'pt': 'Configurações do adaptador para Seq',
+		'nl': 'Adapterinstellingen voor Seq',
+		'fr': "Paramètres d'adaptateur pour Seq",
+		'it': "Impostazioni dell'adattatore per Seq",
+		'es': 'Ajustes del adaptador para Seq',
+		'pl': 'Ustawienia adaptera dla Seq',
 		'zh-cn': 'seq的适配器设置'
 	},
 	'serverData': {
 		'en': 'Server Data',
-		'de': 'Server Data'
+		'de': 'Server Data',
+		'ru': 'данные сервера'
 	},
 	'logLvl': {
 		'en': 'Log level',
-		'de': 'Log Level'
+		'de': 'Log Level',
+		'ru': 'уровень регистрации'
 	},
 	'silly': {
 		'en': 'Verbose',
-		'de': 'Verbose'
+		'de': 'Verbose',
+		'ru': 'Вербальный'
 	},
 	'debug': {
 		'en': 'Debug',
-		'de': 'Debug'
+		'de': 'Debug',
+		'ru': 'Отладка'
 	},
 	'info': {
 		'en': 'Information',
-		'de': 'Information'
+		'de': 'Information',
+		'ru': 'Информация'
 	},
 	'warn': {
 		'en': 'Warning',
-		'de': 'Warning'
+		'de': 'Warning',
+		'ru': 'Предупреждение'
 	},
 	'error': {
 		'en': 'Error',
-		'de': 'Error'
+		'de': 'Error',
+		'ru': 'Ошибка'
 	},
 	'apiKey': {
 		'en': 'APIKey (Optional)',
-		'de': 'APIKey (Optional)'
+		'de': 'APIKey (Optional)',
+		'ru': 'APIKey (опционально)'
 	},
 	'url': {
 		'en': 'URL',
-		'de': 'URL'
+		'de': 'URL',
+		'ru': 'URL'
 	},
 	'port': {
 		'en': 'Port',
-		'de': 'Port'
+		'de': 'Port',
+		'ru': 'Порт'
+	},
+	'messageTemplate': {
+		'en': 'Message Template',
+		'de': 'Message Template',
+		'ru': 'шаблон сообщения'
+	},
+	'template': {
+		'en': 'Template',
+		'de': 'Template',
+		'ru': 'Шаблон'
+	},
+	'minimalTemplate': {
+		'en': 'Minimal Template: {Message}',
+		'de': 'Minimales Template: {Message}',
+		'ru': 'Минимальный шаблон: {Message}'
+	},
+	'templateParameters': {
+		'en': 'Template Parameters: {Application}, {Message}, {Pid} and {Source}',
+		'de': 'Template Parameter: {Application}, {Message}, {Pid} und {Source}',
+		'ru': 'Параметры шаблона: {Application}, {Message}, {Pid} и {Source}.'
 	},
 };
